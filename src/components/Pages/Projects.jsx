0c0c4aa6ad3ee934ddba30e9d71e2a72fb8edc78
Prajwal Kumar <kumpraj@gmail.com>
2023-02-09 22:26:15 +0530

    updated project component

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -2,18 +2,67 @@
 
 function Projects() {
   return (
-  <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-      <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
+  <div className="hero mt-12">  
+    <div className="mx-auto grid gap-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="card w-96 glass">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Check Out!</button>
+            </div>
+          </div>
       </div>
+
+      <div className="card w-96 glass">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Check Out!</button>
+            </div>
+          </div>
       </div>
+
+      <div className="card w-96 glass">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Check Out!</button>
+            </div>
+          </div>
+      </div>
+
+      <div className="card w-96 glass">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Check Out!</button>
+            </div>
+          </div>
+      </div>
+
+      <div className="card w-96 glass">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Check Out!</button>
+            </div>
+          </div>
+      </div>
+
+    </div>
   </div>
+  
+
   )
 }
 
