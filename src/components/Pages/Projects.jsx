96b2d33c63292f5850f946168a2120ab5ab12308
Prajwal Kumar <kumpraj@gmail.com>
2023-02-14 23:49:16 +0530

    added card for github finder app in projects

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -4,6 +4,7 @@ import pic5 from '../../img/proj_images/5.png'
 import pic8 from '../../img/proj_images/8.png'
 import pic9 from '../../img/proj_images/9.png'
 import pic15 from '../../img/proj_images/15.png'
+import ghf01 from '../../img/proj_images/githubfinder01.PNG'
 
 
 
@@ -12,11 +13,14 @@ function Projects() {
   <div className="hero mt-12">  
     <div className="mx-auto grid gap-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
 
+      <Card title='Github Finder App' desc='Github profile finder app created using React JS' url={ghf01} link='https://kumpraj-github-finder-app.vercel.app/'/>
+      
       <Card title='card-2' desc='desc-2' url={pic2} link='https://kumpraj-html-css-proj-02.netlify.app/'/>
       <Card title='card-5' desc='desc-5' url={pic5} link='https://kumpraj-html-css-proj-05.netlify.app/'/>
       <Card title='card-8' desc='desc-8' url={pic8} link='https://kumpraj-html-css-proj-08.netlify.app/'/>
       <Card title='card-9' desc='desc-9' url={pic9} link='https://kumpraj-html-css-proj-09.netlify.app/'/>
       <Card title='card-15' desc='desc-15' url={pic15} link='https://kumpraj-html-css-proj-15.netlify.app/'/>
+
       
       
 
