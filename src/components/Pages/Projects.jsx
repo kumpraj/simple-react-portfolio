a89c2dce8e8a8bf2171374c5580c42339c8b3b12
Prajwal Kumar <kumpraj@gmail.com>
2023-02-15 23:26:10 +0530

    add todo app in projects

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -5,6 +5,9 @@ import pic8 from '../../img/proj_images/8.png'
 import pic9 from '../../img/proj_images/9.png'
 import pic15 from '../../img/proj_images/15.png'
 import ghf01 from '../../img/proj_images/githubfinder01.PNG'
+import todo01 from '../../img/proj_images/todoapp.PNG'
+
+
 
 
 
@@ -14,6 +17,8 @@ function Projects() {
     <div className="mx-auto grid gap-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
 
       <Card title='Github Finder App' desc='Github profile finder app created using React JS' url={ghf01} link='https://kumpraj-github-finder-app.vercel.app/'/>
+
+      <Card title='FullStack Todo App' desc='Todo app created using MERN stack' url={todo01} link='https://github.com/kumpraj/todo_app'/>
       
       <Card title='card-2' desc='desc-2' url={pic2} link='https://kumpraj-html-css-proj-02.netlify.app/'/>
       <Card title='card-5' desc='desc-5' url={pic5} link='https://kumpraj-html-css-proj-05.netlify.app/'/>
@@ -21,6 +26,7 @@ function Projects() {
       <Card title='card-9' desc='desc-9' url={pic9} link='https://kumpraj-html-css-proj-09.netlify.app/'/>
       <Card title='card-15' desc='desc-15' url={pic15} link='https://kumpraj-html-css-proj-15.netlify.app/'/>
 
+      
       
       
 
