3eee7ac8f951c82730a5ec7efc2020a79f8b97e4
Prajwal Kumar <kumpraj@gmail.com>
2023-02-16 22:40:30 +0530

    added form submission via getform.io

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 
 
-function Projects() {
+function Contact() {
     return (
 <div className="hero min-h-screen bg-base-200">
   <div className="hero-content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
@@ -14,39 +14,41 @@ function Projects() {
       </p>
     </div>
     <div className="mx-auto card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-          <input type="text" placeholder="Name" className="input input-bordered" />
+      <form action="https://getform.io/f/bbb6cd5e-bf59-4517-ae6f-274803130a75" method="POST">
+        <div className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input type="text" name="name" placeholder="Name" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" name="email" placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Subject</span>
           </label>
-          <input type="text" placeholder="Subject" className="input input-bordered" />
+          <input type="text" name="subject" placeholder="Subject" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Message</span>
           </label>
-          <textarea type="text" placeholder="Message" className="textarea textarea-bordered" />
+          <textarea type="text" name="message" placeholder="Message" className="textarea textarea-bordered" />
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Send</button>
+          <button type="submit" className="btn btn-primary">Send</button>
         </div>
       </div>
+      </form>
     </div>
   </div>
 </div>
     )
   }
   
-  export default Projects
+  export default Contact
